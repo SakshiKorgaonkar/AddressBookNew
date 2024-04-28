1b83be81c1acf84ecce7ae7e39ff9ac67eebf4ab
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-04-28 20:10:10 +0530

    [sakshi]Add :contact class created

---
+++ AddressBookNew/AddressBookNew/Contact.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookNew
+{
+    internal class Contact
+    {
+        private string? firstName;
+        private string? lastName;
+        private string? email;
+        private string? phone;
+        private string? address;
+        private string? city;
+        private string? state;
+        private string? zip;
+
+        public void acceptRecord()
+        {
+            Console.WriteLine("Enter First Name : ");
+            firstName=Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            lastName=Console.ReadLine();
+            Console.WriteLine("Enter email : ");
+            email=Console.ReadLine();
+            Console.WriteLine("Enter phone number : ");
+            phone=Console.ReadLine();
+            Console.WriteLine("Enter address : ");
+            address=Console.ReadLine();
+            Console.WriteLine("Enter city : ");
+            city=Console.ReadLine();
+            Console.WriteLine("Enter state : ");
+            state=Console.ReadLine();
+            Console.WriteLine("Enter zip : ");
+            zip=Console.ReadLine();
+        }
+        public void printRecord()
+        {
+            Console.WriteLine($"First Name : {firstName}");
+            Console.WriteLine($"Last Name : {lastName}");
+            Console.WriteLine($"Email : {email}");
+            Console.WriteLine($"Address : {address}");
+            Console.WriteLine($"City : {city}");
+            Console.WriteLine($"State : {state}");
+            Console.WriteLine($"ZIP code : {zip}");
+            Console.WriteLine($"Phone number : {phone}");
+        }
+    }
+}
--- AddressBookNew/AddressBookNew/obj/Debug/net6.0/AddressBookNew.AssemblyInfo.cs
+++ AddressBookNew/AddressBookNew/obj/Debug/net6.0/AddressBookNew.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookNew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67197dd0211bf48eb205f0501c13aa3aa602d8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookNew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookNew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
