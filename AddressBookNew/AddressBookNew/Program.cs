67197dd0211bf48eb205f0501c13aa3aa602d8ed
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-04-28 19:54:13 +0530

    [sakshi]Add :on master branch welcome message

---
+++ AddressBookNew/AddressBookNew/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace AddressBookNew
+{
+    internal class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
